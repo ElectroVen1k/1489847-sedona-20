f4c4af4656b7646e834fbe3a291b45694b5f71b1
ElectroVen1k <65223599+ElectroVen1k@users.noreply.github.com>
2020-09-02 00:13:06 +0400

    Исправил замечания

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,7 @@ const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const svgstore = require('gulp-svgstore');
-
 // Styles
-
 const styles = () => {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -22,17 +20,16 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
-    .pipe(sync.stream());
+    .pipe(sync.stream())
 }
-
 exports.styles = styles;
-
 // Server
-
 const server = (done) => {
   sync.init({
     server: {
@@ -44,63 +41,53 @@ const server = (done) => {
   });
   done();
 }
-
 exports.server = server;
-
 // Watcher
-
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
-
 exports.default = gulp.series(
   styles, server, watcher
 );
-
 //Clean
-
 const clean = () => {
   return del("build");
 };
-
 exports.clean = clean;
-
 //Copy
-
 const copy = () => {
   return gulp.src([
-    "source/img/**",
-    "source/js/**/*.js",
-    "source/fonts/**/*.{woff,woff2}",
-    "source/*.html"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
+      "source/img/**",
+      "source/js/**/*.js",
+      "source/fonts/**/*.{woff,woff2}",
+      "source/*.html"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
 };
-
 exports.copy = copy;
-
 //Webp
-
 const convertwebp = () => {
   return gulp.src("source/img/**/*.{png,jpg}")
-  .pipe(webp())
-  .pipe(gulp.dest("source/img"));
+    .pipe(webp())
+    .pipe(gulp.dest("source/img"));
 };
-
 exports.convertwebp = convertwebp;
-
 //Sprite
-
 const sprite = () => {
   return gulp.src("source/img/**/icon-*.svg")
-  .pipe(svgstore())
-  .pipe(rename("sprite.svg"))
-  .pipe(gulp.dest("build/img"));
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 };
-
 exports.sprite = sprite;
-
 //Build
+const build = gulp.series(
+  clean,
+  copy,
+  sprite,
+  styles
+);
+exports.build = build;
